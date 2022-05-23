f78d5a6f3a44724016e2ce6f67239667e4186f77
Rosemary Liu <rliu18@gmail.com>
2022-05-23 10:37:36 -0700

    Completed 242 valid anagram

--- 242-valid-anagram.js
+++ 242-valid-anagram.js
@@ -7,6 +7,52 @@
 // @param {string} t
 // @return {boolean}
 
+// my solution 5/23/22
+
 const isAnagram = (s, t) => {
-  
+  if (s.length !== t.length) {
+    return false;
+  }
+
+  const sArray = [];
+  const tArray = [];
+
+  for (let i = 0; i < s.length; i++) {
+    sArray.push(s[i]);
+    tArray.push(t[i]);
+  }
+
+  sSorted = sArray.sort();
+  tSorted = tArray.sort();
+
+  for (let j = 0; j < sSorted.length; j++) {
+    if (sSorted[j] !== tSorted[j]){
+      return false;
+    }
+  }
+  return true;
+}
+
+
+// recommended solution using map
+
+const isAnagram = (s, t) => {
+  if (s.length !== t.length) {
+    return false;
+  }
+
+  const counts = {};
+  for (let c in s) {
+    // if counts[c] doesn't exist yet, start count at 0
+    counts[c] = (counts[c] || 0) + 1
+  }
+  for (let c in t) {
+    // if count doesn't exist yet, is false
+    if (!counts[c]) {
+      return false;
+    }
+    // if count exists, decrement it
+    counts[c]--;
+  }
+  return true;
 }
