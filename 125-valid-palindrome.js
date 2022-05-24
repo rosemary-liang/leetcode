10ccba8380a694c9852fd7e8e49050d785cea8bd
Rosemary Liu <rliu18@gmail.com>
2022-05-24 15:40:08 -0700

    Completed 125-valid-palindrome

--- 125-valid-palindrome.js
+++ 125-valid-palindrome.js
@@ -8,6 +8,38 @@
 //  @param {string} s
 //   @return {boolean}
 
+// my solution 5/24/22
 const isPalindrome = (s) => {
+  const removeSpecialChars = s.replace(/[^a-z0-9]/gi, '');
+  const removeUnderscores = removeSpecialChars.replace('_', '');
+  const lowerCased = removeUnderscores.toLowerCase();
 
+  if (lowerCased.length % 2 === 0) {
+    for (let i = 0; i < (lowerCased.length / 2); i++) {
+      if (lowerCased[i] !== lowerCased[lowerCased.length - 1 - i]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    for (let i = 0; i < Math.floor((lowerCased.length / 2)); i++) {
+      if (lowerCased[i] !== lowerCased[lowerCased.length - 1 - i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+
+
+// simple solution
+// lowercase all and then replace all non-alphanumeric
+const isPalindrome = (s) => {
+  s = s.toLowerCase().replace(/[^a-z0-9]/gi, '');
+  for (let i = 0, j = s.length - 1; i <= j; i++, j--) {
+    if (s[i] !== s[j]) {
+      return false;
+    }
+  }
+  return true;
 }
