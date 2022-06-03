15769ea0c8d89f298495677394210948f5777a10
Rosemary Liu <rliu18@gmail.com>
2022-06-03 14:03:12 -0700

    Completed js-bank exercise

--- javascript-bank/account.js
+++ javascript-bank/account.js
@@ -1,50 +1,14 @@
 /* exported Account */
-// class Account {
-//   constructor(number, holder) {
-//     this.number = number;
-//     this.holder = holder;
-//     this.transactions = [];
-//   }
+class Account {
+  constructor(number, holder) {
+    this.number = number;
+    this.holder = holder;
+    this.transactions = [];
+  }
 
-//   deposit(amount) {
-//     if (Number.isInteger(amount) && amount > 0) {
-//       const deposit = new Transaction('deposit', amount)
-//       this.transactions.push(deposit);
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   }
-
-//   withdraw(amount) {
-//     if (Number.isInteger(amount) && amount < 0) {
-//       const withdrawal = new Transaction('withdrawal', amount);
-//       this.transactions.push(withdrawal);
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   }
-
-//   getBalance() {
-//     if this.transactions.length < 1 return 0
-//     if (this.transactions.length >= 1) {
-//       const deposits = this.transactions.filter('deposit');
-
-//     }
-//   }
-// }
-
-function Account(number, holder) {
-  this.number = number;
-  this.holder = holder;
-  this.transactions = [];
-}
-
-
-Account.prototype.deposit = function(amount) {
+  deposit(amount) {
     if (Number.isInteger(amount) && amount > 0) {
-      const deposit = new Transaction('deposit', amount);
+      const deposit = new Transaction('deposit', amount)
       this.transactions.push(deposit);
       return true;
     } else {
@@ -52,7 +16,7 @@ Account.prototype.deposit = function(amount) {
     }
   }
 
-Account.prototype.withdraw = function(amount) {
+  withdraw(amount) {
     if (Number.isInteger(amount) && amount < 0) {
       const withdrawal = new Transaction('withdrawal', amount);
       this.transactions.push(withdrawal);
@@ -62,10 +26,57 @@ Account.prototype.withdraw = function(amount) {
     }
   }
 
-Account.prototype.getBalance = function() {
-    if this.transactions.length < 1 return 0
+  getBalance() {
+    let depositValue = 0;
+    let withdrawalValue = 0;
+    let balance = 0;
+    if this.transactions.length < 1 return balance
     if (this.transactions.length >= 1) {
-      const deposits = this.transactions.filter('deposit');
-
+      for (let i = 0; i < this.transactions.length; i++) {
+        if (this.transactions[i].type === 'deposit') {
+          depositValue += deposits[i].amount;
+        }
+        if (this.transactions[i].type === 'withdrawal') {
+          withdrawalValue += withdrawals[i].amount;
+        }
+      }
     }
+    const balance = depositValue - withdrawalValue;
+    return balance;
   }
+}
+
+// function Account(number, holder) {
+//   this.number = number;
+//   this.holder = holder;
+//   this.transactions = [];
+// }
+
+
+// Account.prototype.deposit = function(amount) {
+//     if (Number.isInteger(amount) && amount > 0) {
+//       const deposit = new Transaction('deposit', amount);
+//       this.transactions.push(deposit);
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+
+// Account.prototype.withdraw = function(amount) {
+//     if (Number.isInteger(amount) && amount < 0) {
+//       const withdrawal = new Transaction('withdrawal', amount);
+//       this.transactions.push(withdrawal);
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   }
+
+// Account.prototype.getBalance = function() {
+//     if this.transactions.length < 1 return 0
+//     if (this.transactions.length >= 1) {
+//       const deposits = this.transactions.filter('deposit');
+
+//     }
+//   }
--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -1,1 +1,37 @@
 /* exported Bank */
+class Bank {
+  constructor() {
+    this.nextAccountNumber = this.nextAccountNumber;
+    this.accounts = [];
+  }
+
+  openAccount(holder, balance) {
+    if (Number.isInteger(balance) && balance > 0) {
+      const newAccount = new Account(this.nextAccountNumber, holder);
+      newAccount.deposit(balance);
+      this.accounts.push(newAccount);
+      this.nextAccountNumber++;
+      return newAccount.number;
+    }
+  }
+
+  getAccount(number) {
+    if (Number.isInteger(number)) {
+    for (let i = 0; i < this.accounts.length; i++) {
+      if (this.accounts[i].number === number) {
+        return this.accounts[i];
+        }
+      }
+    }
+    return null
+  }
+
+  getTotalAssets() {
+    let totalAssets = 0;
+    for (let accounts of this.accounts) {
+      const balance = accounts.getBalance();
+      totalAssets += balance;
+    }
+    return totalAssets;
+  }
+}
