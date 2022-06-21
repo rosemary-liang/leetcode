4ed15d845185ce191b95f7178f65dbed6eda4e69
Rosemary Liu <rliu18@gmail.com>
2022-06-21 20:04:14 +0000

    linked list 2 unshift shift get set

--- udemy-dsa-course/linked-list-2.js
+++ udemy-dsa-course/linked-list-2.js
@@ -46,4 +46,56 @@ class LinkedList {
     this.length--;
     return temp;
   }
+
+  unshift(value) {
+    // edge: empty nonempty
+    const newNode = new Node(value);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  shift() {
+    // edge: empty, 1, multi
+    if (this.length === 0) return undefined;
+    let temp = this.head;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.head = this.head.next;
+      temp.next = null;
+    }
+    this.length--;
+    return temp;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    let temp = this.head;
+    for (let i = 0; i < index; i++) {
+      temp = temp.next;
+    }
+    return temp;
+  }
+
+  set(index, value) {
+    // edge: empty, invalid index
+    let temp = this.get();
+    if (temp) {
+      temp.value = value;
+      return true;
+    }
+    return false;
+  }
+
+  insert(index, value) {
+    
+  }
 }
