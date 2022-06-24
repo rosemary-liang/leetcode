00293bd0ee2c9a603923791ff3d3c36ddeb365a3
Rosemary Liu <rliu18@gmail.com>
2022-06-24 15:49:35 -0700

    more linked-list-2 practice

--- udemy-dsa-course/linked-list-2.js
+++ udemy-dsa-course/linked-list-2.js
@@ -27,6 +27,30 @@ class LinkedList {
     return this;
   }
 
+  push(value) {
+    // edge, empty vs non empty
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode; // connect NN
+      this.tail = newNode; // reassign tail to NN
+    }
+  }
+
+  push(value) {
+    // edge: empty vs nonempty
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
   pop() {
     // edge: empty, 1, multi
     if (this.length === 0) return undefined;
@@ -47,9 +71,63 @@ class LinkedList {
     return temp;
   }
 
+  pop() {
+    let temp = this.head;
+    let pre = this.head;
+
+    if (this.length === 0) return undefined;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      while (temp.next) {
+        pre = temp;
+        temp = temp.next;
+      }
+      this.tail = pre;
+      this.tail.next = null;
+    }
+    this.length--;
+    return temp;
+  }
+
+  pop() {
+    let temp = this.head;
+    let prev = this.head;
+    if (this.length === 0) return undefined;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      while (temp.next) {
+        prev = temp;
+        temp = temp.next;
+      }
+      this.tail = prev;
+      this.tail.next = null;
+    }
+    this.length--;
+    return temp;
+  }
+
   unshift(value) {
     // edge: empty nonempty
     const newNode = new Node(value);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  unshift(value) {
+    // edge empty nonempty
+    const newNode = new Node(value);
+
     if (this.length === 0) {
       this.head = newNode;
       this.tail = newNode;
@@ -127,6 +205,56 @@ class LinkedList {
   }
 
   reverse() {
-    
+    // need 3 pointers prev temp next
+
+    let temp = this.head;
+
+    // reverse h & t
+    this.head = this.tail;
+    this.tail = temp;
+    let next = temp.next; // initialize as head.next
+    let prev = null; // initialize as null
+
+    for (let i = 0; i < this.length; i++) {
+      next = temp.next; // move next to NN; 1st already true. 2nd gives access to NN
+      temp.next = prev; // reverse pointer
+      prev = temp; // move prev to NN
+      temp = next; // move temp to next
+    }
+    return this;
+  }
+
+  reverse() {
+    // 3 pointers, prev, temp, next
+    let temp = this.head;
+    this.head = this.tail;
+    this.tail = this.head;
+    let next = temp.next;
+    let prev = null;
+
+    for (let i = o; i < this.length; i++) {
+      next = temp.next; // move next to give use access to NN
+      temp.next = prev; // reverse pointer direction
+      prev = temp; // move prev to next node
+      temp = next; // move temp to next node
+    }
+    return this;
+  }
+
+  reverse() {
+    // 3 pointers prev temp next
+    let temp = this.head;
+    this.head = this.tail;
+    this.tail = this.head;
+    let prev = null;
+    let next = temp.next;
+
+    for (let i = 0; i < this.length; i++) {
+      next = temp.next;
+      temp.next = prev;
+      prev = temp;
+      temp = next;
+    }
+    return this;
   }
 }
