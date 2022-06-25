eab07084e1fbb6cee555fcab15ff7a4b95266dfe
Rosemary Liu <rliu18@gmail.com>
2022-06-25 11:30:20 -0700

    leetcode 13-roman-to-integer

--- leetcode/1-two-sum.js
+++ leetcode/1-two-sum.js
@@ -4,17 +4,15 @@
 
 // You can return the answer in any order.
 
-
 //  @param {number[]} nums
 //  @param {number} target
 //  @return {number[]}
-
 
 const twoSum = (nums, target) => {
   const results = [];
   for (let i = 0; i < nums.length; i++) {
     const diff = target - nums[i];
-    for (let j = (i + 1); j < nums.length; j++) {
+    for (let j = i + 1; j < nums.length; j++) {
       if (nums[j] === diff) {
         results.push(i);
         results.push(j);
@@ -22,4 +20,24 @@ const twoSum = (nums, target) => {
       }
     }
   }
-}
+};
+
+// 6/25/22
+// use a Map to hold element in nums array and index
+// use a loop to iterate over nums array
+// for each element, set variable complement = target - nums[i]
+// if map.has(complement) is true, return the indices
+// else map.set(nums[i], i)
+const twoSum = (nums, target) => {
+  let map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    const complement = target - nums[i];
+    if (map.has(complement)) {
+      return [map.get(complement), i];
+    } else {
+      map.set(nums[i], i);
+    }
+  }
+  return [];
+};
