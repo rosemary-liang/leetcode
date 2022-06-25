67a3d5d9a1875324b2adaaa8a4018ac49d17c8ff
Rosemary Liu <rliu18@gmail.com>
2022-06-24 17:04:49 -0700

    hash table get and keys

--- udemy-dsa-course/hash-tables.js
+++ udemy-dsa-course/hash-tables.js
@@ -17,6 +17,30 @@ class HashTable {
       this.dataMap[index] = [];
     }
     this.dataMap[index].push([key, value]);
-    return this; 
+    return this;
+  }
+
+  get(key) {
+    let index = this._hash(key);
+    if (this.dataMap[index]) {
+      for (let i = 0; i < this.dataMap[index].length; i++) {
+        if (this.dataMap[index][i][0] === key) {
+          return this.dataMap[index][i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+
+  keys() {
+    let allKeys = [];
+    for (let i = 0; i < this.dataMap.length; i++) {
+      if (this.dataMap[i]) {
+        for (let j = 0; j < this.dataMap[i].length; j++) {
+          allKeys.push(this.dataMap[i][j][0]);
+        }
+      }
+    }
+    return allKeys;
   }
 }
