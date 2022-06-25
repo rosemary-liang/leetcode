d92ea6d4ae56a34da80bb4d86d7fdd2319989c62
Rosemary Liu <rliu18@gmail.com>
2022-06-25 10:10:44 -0700

    hashtable example interview question

--- udemy-dsa-course/hash-tables.js
+++ udemy-dsa-course/hash-tables.js
@@ -3,6 +3,7 @@ class HashTable {
     this.dataMap = new Array(size);
   }
 
+  // O(1)
   _hash(key) {
     let hash = 0;
     for (let i = 0; i < key.length; i++) {
@@ -11,6 +12,7 @@ class HashTable {
     return hash;
   }
 
+  // O(1)
   set(key, value) {
     let index = this._hash(key);
     if (!this.dataMap[index]) {
@@ -20,6 +22,9 @@ class HashTable {
     return this;
   }
 
+  // O(1) if can find in one operation
+  // (disregard because below) O(n) worst case if have to go through linked list at index
+  // hash is efficient at hashing at diff indexes, so get is considered O(1)!!!
   get(key) {
     let index = this._hash(key);
     if (this.dataMap[index]) {
@@ -44,3 +49,35 @@ class HashTable {
     return allKeys;
   }
 }
+
+//O(n)^2
+function itemInCommon(arr1, arr2) {
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) return true;
+    }
+  }
+  return false;
+}
+
+let array1 = [1, 3, 5];
+let array2 = [2, 4, 5];
+
+itemInCommon(array1, array2);
+
+// O(n)
+function itemInCommon(arr1, arr2) {
+  let obj = {};
+  for (let i = 0; i < arr1.length; i++) {
+    obj[arr1[i]] = true; // set all items in arr1 as keys in object
+  }
+  for (let j = 0; j < arr2.length; j++) {
+    if (obj[arr2[j]]) return true; // if item in arr2 is a key in object, return true
+  }
+  return false;
+}
+
+let array1 = [1, 3, 5];
+let array2 = [2, 4, 5];
+
+itemInCommon(array1, array2);
