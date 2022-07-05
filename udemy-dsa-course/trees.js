8bfd8072f9e3d3403b93df59ea7f5b0d4ace4115
Rosemary Liu <rliu18@gmail.com>
2022-07-05 14:15:46 -0700

    BST constructor and insert

---
+++ udemy-dsa-course/trees.js
@@ -0,0 +1,43 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    //edge: null root
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+
+    let temp = this.root;
+
+    while (true) {
+      if (newNode.value === temp.value) return undefined; // cannot have equal values
+      if (newNode.value < temp.value) {
+        if (temp.left === null) {
+          temp.left = newNode;
+          return this;
+        } else {
+          temp = temp.left;
+        }
+      } else {
+        if (temp.right === null) {
+          temp.right = newNode;
+          return this;
+        } else {
+          temp = temp.right;
+        }
+      }
+    }
+  }
+}
