c441cafaa8419a4e601d9d4e139a5f20dd370274
Rosemary Liu <rliu18@gmail.com>
2022-07-05 15:13:14 -0700

    graphs addEdge

--- udemy-dsa-course/graphs.js
+++ udemy-dsa-course/graphs.js
@@ -8,6 +8,15 @@ class Graph {
       this.adjacencyList[vertex] = [];
       return true;
     }
+    return false; // will not add a duplicate vertex
+  }
+
+  addEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) { // both vertices must exist to add edge
+      this.adjacencyList[vertex1].push(vertex2);
+      this.adjacencyList[vertex2].push(vertex1);
+      return true;
+    }
     return false;
   }
 }
