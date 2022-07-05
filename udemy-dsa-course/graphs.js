7bea4a228925a1da285a620fd4e9028514ed3993
Rosemary Liu <rliu18@gmail.com>
2022-07-05 15:17:54 -0700

    graphs removeEdge

--- udemy-dsa-course/graphs.js
+++ udemy-dsa-course/graphs.js
@@ -12,11 +12,27 @@ class Graph {
   }
 
   addEdge(vertex1, vertex2) {
-    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) { // both vertices must exist to add edge
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      // both vertices must exist to add edge
       this.adjacencyList[vertex1].push(vertex2);
       this.adjacencyList[vertex2].push(vertex1);
       return true;
     }
     return false;
+  }
+
+  removeEdge(vertex1, vertex2) {
+    if (this.adjacencyList[vertex1] && this.adjacencyList[vertex2]) {
+      this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+        // return all items not equal to vertex2 (removes the edge)
+        (v) => v !== vertex2
+      );
+
+      this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+        (v) => v !== vertex1
+      );
+      return true;
+    }
+    return false; // if both vertices don't exist
   }
 }
