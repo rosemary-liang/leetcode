9e911004b1ea39a594caffdd53cf80f6cd807758
Rosemary Liu <rliu18@gmail.com>
2022-07-07 10:26:23 -0700

    practice valid palindrome

--- leetcode/string/125-valid-palindrome.js
+++ leetcode/string/125-valid-palindrome.js
@@ -8,38 +8,69 @@
 //  @param {string} s
 //   @return {boolean}
 
-// my solution 5/24/22
-const isPalindrome = (s) => {
-  const removeSpecialChars = s.replace(/[^a-z0-9]/gi, '');
-  const removeUnderscores = removeSpecialChars.replace('_', '');
-  const lowerCased = removeUnderscores.toLowerCase();
-
-  if (lowerCased.length % 2 === 0) {
-    for (let i = 0; i < (lowerCased.length / 2); i++) {
-      if (lowerCased[i] !== lowerCased[lowerCased.length - 1 - i]) {
-        return false;
-      }
-    }
-    return true;
-  } else {
-    for (let i = 0; i < Math.floor((lowerCased.length / 2)); i++) {
-      if (lowerCased[i] !== lowerCased[lowerCased.length - 1 - i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-}
-
-
 // simple solution
 // lowercase all and then replace all non-alphanumeric
 const isPalindrome = (s) => {
-  s = s.toLowerCase().replace(/[^a-z0-9]/gi, '');
+  s = s.toLowerCase().replace(/[^a-z0-9]/gi, ""); // regex, choose any char that is not a-z0-9, global, case insensitive
   for (let i = 0, j = s.length - 1; i <= j; i++, j--) {
     if (s[i] !== s[j]) {
       return false;
     }
   }
   return true;
-}
+};
+
+/* 7/7/22
+return boolean
+edge: special nonalphanumeric chars are removed, all lowercased
+
+lowercase the string, remove nonalphanumeric chars
+
+loop through string
+  initialize i at 0
+  initialize j at length-1
+
+  loop until i meets j in the middle
+  increment i, decrement j
+
+if chars at i and j don't match, return false
+
+exit loop
+return true
+*/
+
+const isPalindrome = (s) => {
+  s = s.toLowerCase().replace(/[^a-z0-9]/gi, "");
+
+  for (let i = 0, j = s.length - 1; i <= j; i++ j--) {
+    if (s[i] !== s[j]) return false;
+  }
+  return true
+};
+
+/* 7/7/22 part 2
+return boolean
+input: any string
+
+convert to lowercase, remove all non alphanumeric
+
+loop through string
+  initialize i at 0 and j at last index
+  condition when i and j meet
+  i++ j--
+
+  if char at i and j dont match, return false
+
+exit loop
+return true
+*/
+
+const isPalindrome = (s) => {
+  s = s.toLowerCase().replace(/[^a-z0-9]/gi, "");
+
+  for (let i = 0, j = s.length - 1; i <= j; i++, j--) {
+    if (s[i] !== s[j]) return false;
+  }
+
+  return true;
+};
