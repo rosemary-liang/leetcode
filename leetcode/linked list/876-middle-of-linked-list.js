fb3fe2da9fb241ea909ea6520b516260cbece12a
Rosemary Liu <rliu18@gmail.com>
2022-07-08 17:36:48 -0700

    completed middle of linked list

---
+++ leetcode/linked list/876-middle-of-linked-list.js
@@ -0,0 +1,52 @@
+/*876. Middle of the Linked List
+
+Given the head of a singly linked list, return the middle node of the linked list.
+If there are two middle nodes, return the second middle node.
+
+Example 1:
+Input: head = [1,2,3,4,5]
+Output: [3,4,5]
+Explanation: The middle node of the list is node 3.
+
+Example 2:
+Input: head = [1,2,3,4,5,6]
+Output: [4,5,6]
+Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one. */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+
+7/8/22
+2 pointers, fast and slow, fast moves 2x as fast
+when fast moves to end of LL, slow will be in the middle
+
+[1,2,3,4,5]
+[1,2,3,4]
+
+initialize both fast and slow to head
+while(fast & fast.next)
+ move pointers until break out of loop
+
+ exit loop
+ return slow
+*/
+
+var middleNode = function (head) {
+  let fast = head;
+  let slow = head;
+
+  while (fast && fast.next) {
+    fast = fast.next.next;
+    slow = slow.next;
+  }
+
+  return slow;
+};
