f9e317bfa8223d5b0e859eac4dc09872fb51862a
Rosemary Liu <rliu18@gmail.com>
2022-07-12 10:28:21 -0700

    udemy basic sort

---
+++ udemy-dsa-course/basic-sort.js
@@ -0,0 +1,48 @@
+//bubble sort
+function bubbleSort(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (array[j] > array[j + 1]) {
+        let temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+}
+
+// selection sort
+function selectionSort(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    let min = i;
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] < array[min]) {
+        min = j;
+      }
+    }
+    if (i !== min) {
+      let temp = array[i];
+      array[i] = array[min];
+      array[min] = temp;
+    }
+  }
+  return array;
+}
+
+selectionSort([4, 2, 6, 5, 1, 3]);
+
+// insertion sort
+// always start with 2nd item, compare with item before it, before it, switch if needed
+function insertionSort(array) {
+  let temp;
+  for (let i = 1; i < array.length; i++) {
+    temp = array[i];
+    for (var j = i - 1; array[j] > temp && j > -1; j--) {
+      array[j + 1] = array[j];
+    }
+    array[j + 1] = temp;
+  }
+  return array;
+}
+
+insertionSort([4, 2, 6, 5, 1, 3]);
