326e359151dfb75e0f81a4113f06ee6fe16d9a22
Rosemary Liu <rliu18@gmail.com>
2022-07-13 08:29:31 -0700

    udemy quicksort algorithm

--- udemy-dsa-course/quick-sort.js
+++ udemy-dsa-course/quick-sort.js
@@ -4,6 +4,9 @@ function swap(array, firstIndex, secondIndex) {
   array[secondIndex] = temp;
 }
 
+// pivot
+// swap all items one by one btwn swap & i
+// final swap in loop, swap pivot & swapIndex
 function pivot(array, pivotIndex = 0, endIndex = array.length - 1) {
   let swapIndex = pivotIndex;
   for (let i = pivotIndex + 1; i <= endIndex; i++) {
@@ -18,3 +21,15 @@ function pivot(array, pivotIndex = 0, endIndex = array.length - 1) {
 }
 
 pivot([4, 6, 1, 7, 3, 2, 5]);
+
+function quickSort(array, left = 0, right = array.length - 1) {
+  // if statement allows us to exit recursion once sorted
+  if (left < right) {
+    let pivotIndex = pivot(array, left, right); // rearranges array and also RETURNS swapIndex
+    quickSort(array, left, pivotIndex - 1);
+    quickSort(array, pivotIndex + 1, right);
+  }
+  return array;
+}
+
+quickSort([4, 6, 1, 7, 3, 2, 5]);
