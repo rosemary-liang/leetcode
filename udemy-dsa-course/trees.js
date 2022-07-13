29f542ec5f8d2e6ad0729821ce32fe317098dada
Rosemary Liu <rliu18@gmail.com>
2022-07-13 12:12:37 -0700

    udemy DFS PreOrder

--- udemy-dsa-course/trees.js
+++ udemy-dsa-course/trees.js
@@ -64,19 +64,4 @@ class BST {
     }
     return currentNode;
   }
-
-  BFS() {
-    let currentNode = this.root;
-    let results = [];
-    let queue = [];
-    queue.push(currentNode);
-
-    while (queue.length) {
-      currentNode = queue.shift();
-      results.push(currentNode.value);
-      if (currentNode.left) queue.push(currentNode.left);
-      if (currentNode.right) queue.push(currentNode.right);
-    }
-    return results;
-  }
 }
