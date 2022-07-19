12dc8629ebaee322291f511d9e1a48f2491dbb8f
Rosemary Liu <rliu18@gmail.com>
2022-07-19 11:05:08 -0700

    python lists

--- python/python_sandbox_starter/lists.py
+++ python/python_sandbox_starter/lists.py
@@ -1,1 +1,40 @@
 # A List is a collection which is ordered and changeable. Allows duplicate members.
+
+#Create list -- more common way used
+numbers = [1,2,3,4,5]
+fruits = ['Apples', 'Oranges', 'Grapes', 'Pears']
+
+# use constructor -- don't like doing this way
+numbers2 = list((1,2,3,4,5))
+
+# both of those above will create same thing
+
+# get a value
+print(fruits[1])
+print(len(fruits))
+
+
+#append to list
+fruits.append('Mangos')
+
+#remove
+fruits.remove('Grapes')
+
+#insert into index
+fruits.insert(2, 'Strawbs')
+
+#change value
+fruits[1] = 'Blueberries'
+
+#reverse
+fruits.reverse()
+
+
+#sort list
+fruits.sort()
+
+#reverse sort
+fruits.sort(reverse=True)
+
+
+print(fruits)
--- python/python_sandbox_starter/strings.py
+++ python/python_sandbox_starter/strings.py
@@ -1,6 +1,61 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
 
+name = 'Jim Bob'
+age = 37
+
+#concatenate -- can only concatenate str; must cast other types to str
+print('Hello, my name is ' + name + ' and I am ' + str(age))
 
 # String Formatting
 
+#arguments by position
+print('My name is {name} and I am {age}'.format(name=name, age=age))
+
+# f strings (py3.6+) -- like JS template literals
+print(f'Hello, my name is {name} and I am {age}')
+
 # String Methods
+s = 'hello world'
+
+#capitalize string (first letter of string)
+print(s.capitalize())
+
+# Make all uppercase
+print(s.upper())
+
+# Make all lower
+print(s.lower())
+
+# Swap case
+print(s.swapcase())
+
+# Get length
+print(len(s))
+
+# Replace
+print(s.replace('world', 'everyone'))
+
+# Count
+sub = 'h'
+print(s.count(sub))
+
+# Starts with
+print(s.startswith('hello'))
+
+# Ends with
+print(s.endswith('d'))
+
+# Split into a list - turn into an array
+print(s.split())
+
+# Find position
+print(s.find('r'))
+
+# Is all alphanumeric
+print(s.isalnum())
+
+# Is all alphabetic
+print(s.isalpha())
+
+# Is all numeric
+print(s.isnumeric())
