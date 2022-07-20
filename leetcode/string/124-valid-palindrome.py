a9b1480b297842fa10effcc219c3e7577d7990a0
Rosemary Liu <rliu18@gmail.com>
2022-07-20 10:55:04 -0700

    python invert binary tree

--- leetcode/string/124-valid-palindrome.py
+++ leetcode/string/124-valid-palindrome.py
@@ -2,29 +2,60 @@
 
 # memory O(n)
 # 2 pointers left and right and move to center
+# set up alphaNum and call using self.alphaNum
 class Solution:
   def isPalindrome(self, s: str) -> bool:
     l, r = 0, len(s) - 1
+    while l < r:
+    #move pointers to skip over nonalphanum chars
+    while l < r and not self.alphaNum(s[l]):
+      l += 1
+    while l < r and not self.alphaNum(s[r]):
+    r -= 1
+
+    # check if pointers are equal, if not return false
+    if s[l].lower() != s[r].lower():
+      return False
+
+    # move pointers toward the middle
+    l += 1
+    r -= 1
+
+  return True
 
   def alphaNum(self, c):
      return (ord('A') <= ord(c) <= ('Z') or
       ord('a') <= ord(c) <= ('z') or
       ord('0') <= ord(c) <= ('9'))
 
-  while l < r:
-    while l < r and not self.alphaNum(s[l]):
-      l += 1
 
-    while l < r and not self.alphaNum(s[r]):
-    r -= 1
 
-    if s[l].lower() != s[r].lower():
-      return False
 
-    l += 1
-    r -= 1
 
-  return True
+#practice
+
+class Solution:
+  def isPalindrome(self, s: str) -> bool:
+    l, r = 0, len(s) - 1
+    while l < r:
+      # skip c if nonalpha
+      while l < r and not self.isalphaNum(s[l]):
+        l += 1
+      while r > l and not self.isalphaNum(s[r]):
+        r -= 1
+
+      if s[l].lower() != s[r].lower():
+        return False
+
+      l, r = l + 1, r - 1
+
+    return True
+
+  def isalphaNum(self, c):
+    return (ord('A') <= ord(c) <= ord('Z') or
+    ord('a') <= ord(c) <= ord('z') or
+    ord('0') <= ord(c) <= ord('9') )
+
 
 
 
